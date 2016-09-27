d96d41b121cbc288f42caf0d004791e2c951be5b
Usow Maxim <usow.maxim2016@yandex.ru>
2016-09-27 15:13:29 +0800

    Style upgrade
    Как и обещал, обновил стиль интерфейса:3

--- entity/modules_money_printer/cl_init.lua
+++ entity/modules_money_printer/cl_init.lua
@@ -12,21 +12,40 @@ surface.CreateFont( "HotFont", {
 
 local test = false
 local HotColor = 0
-local TurnPBC
+local powerPBC
+local Color1 = Color( 0, 0, 0, 200 )
+local Color2 = Color( 70, 70, 70 )
+local Color3 = Color( 200, 200, 200 )
+
+function draw.Circle( x, y, radius, seg, pi )
+	local cir = {}
+
+	table.insert( cir, { x = x, y = y, u = 0.5, v = 0.5 } )
+	for i = 0, seg do
+		local a = math.rad( ( i / seg ) * -pi )
+		table.insert( cir, { x = x - math.sin( a ) * radius, y = y - math.cos( a ) * radius, 
+			u = math.sin( a ) / 2 + 0.5, v = math.cos( a ) / 2 + 0.5 } )
+		--PrintTable(cir)
+	end
+
+	surface.DrawPoly( cir )
+end
+
 
 function ENT:Draw()
 	local _player = LocalPlayer()
 	self:DrawModel()
-	if LocalPlayer():EyePos( ):Distance(self.Entity:GetPos()) > 1000 then return end
+	_self = self
+	if _player:EyePos():Distance( self.Entity:GetPos() ) > 1000 then return end
 	--Upgrade System
-	local Upgrades = self:GetNWInt("Upgrade")
-	if Upgrades == 1 then self:SetColor( Color( 218, 218, 218, 255 ) ) self:SetMaterial("models/shiny", false)
-	elseif Upgrades == 2 then self:SetColor( Color( 255, 242, 0, 255 ) )
-	elseif Upgrades == 3 then self:SetColor( Color( 255, 0, 0, 255 ) )
-	elseif Upgrades == 4 then self:SetColor( Color( 29, 223, 0, 255 ) )
-	elseif Upgrades == 5 then self:SetColor( Color( 0, 242, 242, 255 ) )
+	local Upgrades = self:GetNWInt( "Upgrade" )
+	if Upgrades == 1 then self:SetColor( Color( 218, 218, 218 ) ) self:SetMaterial( "models/shiny", false )
+	elseif Upgrades == 2 then self:SetColor( Color( 255, 242, 0 ) )
+	elseif Upgrades == 3 then self:SetColor( Color( 255, 0, 0 ) )
+	elseif Upgrades == 4 then self:SetColor( Color( 29, 223, 0 ) )
+	elseif Upgrades == 5 then self:SetColor( Color( 0, 242, 242 ) )
 	end
-	local FillBur = math.floor(self:GetNWInt("interval")*math.abs(self:GetNWFloat("ProgressBar")-1))
+	local FillBur = math.abs( self:GetNWFloat( "ProgressBar" ) -1 )
 	local HotBur = nil
 	local Pos = self:GetPos()
 	local Ang = self:GetAngles()
@@ -34,74 +53,125 @@ function ENT:Draw()
 	local owner = self:Getowning_ent()
     owner = (IsValid(owner) and owner:Nick()) or DarkRP.getPhrase("unknown")
 
-	if self:GetNWBool("Cooler") == true then
-		HotBur = Color(7,247,247)
+	if self:GetNWBool( "Cooler" ) == true then
+		HotBur = Color( 7, 247, 247 )
 	else 
-		HotBur = Color(200,200,200)
+		HotBur = Color2
 	end
-	--ON/OFF
-	if self:GetNWBool("turn") then TurnPBC = Color( 255,255,255 )
-	else TurnPBC = Color( 160,160,160 )
-	end
-
-
-	--[[ print(FillBur) --]]
-	--Hot Color(7,247,247)
 
 	Ang:RotateAroundAxis( self:GetAngles():Up(), 90 )
 
 	--cooler = ClientsideModel ("models/props_phx/gears/bevel12.mdl", RENDERGROUP_OPAQUE )
-	
 
-	cam.Start3D2D(Pos *1.0 + Ang:Up() * 11.1, Ang, 0.1)
+	cam.Start3D2D( Pos *1.0 + Ang:Up() * 11.1, Ang, 0.1 )
+		--[[-----------------------------------------
+			Upgrade
+		--]]-----------------------------------------
+		draw.RoundedBox( 10, -152.5, -150, 100, 100, Color1 )
+		draw.RoundedBox( 0, -147.5, -145, 90, 90, Color3 )
+		draw.RoundedBox( 0, -147.5, -55, 90, 
+			-self:GetNWInt( "Upgrade" )*18, Color( 255, 120, 0 ) )
+		draw.SimpleText( self:GetNWInt( "Upgrade" ).."/5", "HotFont", -120, -130, 
+			Color( 2, 142, 155 ), 1, 1 )
 
-		draw.RoundedBox(10,-14*10,-15*10,29*10,6*10,Color(0,0,0,120))
-		draw.RoundedBox(10,-14*10,-8*10,29*10,6*10,Color(0,0,0,120))
-		draw.RoundedBox(10,-14*10,-1*10,29*10,6*10,Color(0,0,0,120))
-		--[[ Stats --]]
-		draw.RoundedBox(10,-14*10,6*10,9*10,4*10,Color(0,0,0,120))
-		draw.RoundedBox(10,-14*10,10.5*10,9*10,4*10,Color(0,0,0,120))
+		--[[-----------------------------------------
+			Hot
+		--]]-----------------------------------------
+		draw.RoundedBox( 10, -152.5, 50, 100, 100, Color1 )
+		draw.RoundedBox( 0, -147.5, 55, 90, 90, Color3 )
+		draw.RoundedBox( 0, -147.5, 55, 90, 
+			self:GetNWInt( "Hot" )*0.9, HotBur )
+		draw.SimpleText( self:GetNWInt( "Hot" ).."°", "HotFont", -120, 130, 
+			Color( 255, 119, 7 ), 1, 1 )
 
-		draw.RoundedBox(0,-13.5*10,6.5*10,8*10,3*10,Color(200,200,200))
-		if self:GetNWInt("Hot") >= 80 then
-			draw.RoundedBox(0,-13.5*10,6.5*10,80,3*10,Color(180,0,0,HotColor)) --HotDanger
-			else
-			draw.RoundedBox(0,-13.5*10,6.5*10,80,3*10,HotBur) --Hot
+		--[[-----------------------------------------
+			Paint
+		--]]-----------------------------------------
+		draw.RoundedBox( 10, 52.5, -150, 100, 100, Color1 )
+		draw.RoundedBox( 0, 57.5, -145, 90, 90, Color3 )
+		draw.RoundedBox( 0, 57.5, -55, 90, 
+			-self:GetNWInt( "paint" )*4.5, Color( 0, 0, 0 ) )
+		draw.SimpleText( self:GetNWInt( "paint" ), "HotFont", 130, -130, 
+			Color( 255, 255, 255 ), 1, 1 )
+
+		--[[-----------------------------------------
+			Paper
+		--]]-----------------------------------------
+		draw.RoundedBox( 10, 52.5, 50, 100, 100, Color1 )
+		draw.RoundedBox( 0, 57.5, 55, 90, 90, Color3 )
+		draw.RoundedBox( 0, 57.5, 55, 90, 
+			self:GetNWInt( "paper" )*9, Color( 255, 255, 255 ) )
+		draw.SimpleText( self:GetNWInt( "paper" ), "HotFont", 130, 130, 
+			Color( 0, 0, 0 ), 1, 1 )
+
+		draw.NoTexture()
+		surface.SetDrawColor( 200,150,255 )
+		draw.Circle( 2.5, 0, 150, 32, 360 )
+		surface.SetDrawColor( 150,255,150 )
+		draw.Circle( 2.5, 0, 140, 32, 360 )
+		surface.SetDrawColor( 180,140,140 )
+		draw.Circle( 2.5, 0, 140, 32, FillBur*360 )
+		if not self:GetNWBool("power") then
+		surface.SetDrawColor( 65, 220, 140 )
+		draw.Circle( 2.5, 0, 140, 32, 360 )
 		end
-		draw.RoundedBox(0,-13.5*10,11*10,8*10,3*10,Color(200,200,200)) --Upgrade
-		draw.RoundedBox(0,-13.5*10,11*10,self:GetNWInt("Upgrade")*16,3*10,Color(255,120,0)) --Upgrade
+		surface.SetDrawColor( 65, 200, 220 )
+		draw.Circle( 2.5, 0, 110, 32, 360 )
 
-		--Paper,Color
-		draw.RoundedBox(10,-4*10,6*10,13.5*10,4*10,Color(0,0,0,120))
-		draw.RoundedBox(10,-4*10,10.5*10,13.5*10,4*10,Color(0,0,0,120))
-
-		draw.RoundedBox(0,-3.5*10,6.5*10,12.5*10,3*10,Color(200,200,200))
-		draw.RoundedBox(0,-3.5*10,11*10,12.5*10,3*10,Color(200,200,200))
-
-		draw.RoundedBox(0,-3.5*10,6.5*10,self:GetNWInt("paint")*6.25,3*10,Color(0,0,0))
-		draw.RoundedBox(0,-3.5*10,11*10,self:GetNWInt("paper")*12.5,3*10,Color(255,255,255))
-
-		--On/OFF
-		draw.RoundedBox(10,10.5*10,6*10,4.5*10,8.5*10,Color(0,0,0,120))
-		draw.RoundedBox(10,11*10,6.5*10,3.5*10,7.5*10,Color(200,200,200))
-		if self:GetNWBool("turn") then draw.RoundedBox(10,11*10,6.5*10,3.5*10,3.75*10,Color(0,0,200))
-		else draw.RoundedBox(10,11*10,10.25*10,3.5*10,3.75*10,Color(200,0,0)) end
-
-		draw.SimpleText( FillBur.." sec", "MoneyFont2", 0, -50, TurnPBC, 1, 1 )
-
-		draw.SimpleText( self:GetNWInt("Upgrade").."/5", "HotFont", -95, 125, Color( 255,255,255 ), 1, 1 )
-		draw.SimpleText( self:GetNWInt("Hot").."°", "HotFont", -95, 80, Color( 255,255,255 ), 1, 1 )
-		draw.SimpleText( owner, "MoneyFont", 0, -120, Color( 255, 120, 0 ), 1, 1 )
-		draw.SimpleText( self:GetMoneyAmount().."$", "MoneyFont2", 0, 20, Color( 0, 200, 0 ), 1, 1 )
+		--[[-----------------------------------------
+			Money
+		--]]-----------------------------------------
+		draw.SimpleText( self:GetMoneyAmount().."$", "MoneyFont2", 2.5, 0, 
+		Color( 0, 100, 0 ), 1, 1 )
 	cam.End3D2D()
-	--And
 	
 	Ang:RotateAroundAxis( Ang:Forward(), 90 )
+	cam.Start3D2D( Pos *1.0 + Ang:Up() * 17, Ang, 0.1 )
+		--[[-----------------------------------------
+		Health
+	    --]]-----------------------------------------
+		draw.RoundedBox( 10, -140, -100, 255, 60, Color2 )
+		draw.RoundedBox( 0, -135, -95, 245, 50, Color3 )
+		draw.RoundedBox( 0, -135, -95, self:GetNWInt( "Health" )*2.45, 50, 
+		Color( 255, 100, 100 ) )
 
-	cam.Start3D2D(Pos *1.0 + Ang:Up() * 17, Ang, 0.1)
-		draw.RoundedBox(10,-14*10,-10*10,29*10,6*10,Color(0,0,0,120))
-		draw.RoundedBox(0,-13.5*10,-9.5*10,28*10,5*10,Color(200,200,200))
-		draw.RoundedBox(0,-13.5*10,-9.5*10,self:GetNWInt("Health")*2.8,5*10,Color(255,100,100))
+		--[[-----------------------------------------
+			Power
+		--]]-----------------------------------------
+		draw.RoundedBox( 0, 120, -100, 34, 60, Color2 )
+		draw.RoundedBox( 0, 124, -96, 26, 52, Color3 )
+		if self:GetNWBool( "power" ) then
+			draw.RoundedBox( 0, 124, -96, 26, 26, Color( 0, 0, 200 ) )
+		else 
+			draw.RoundedBox( 0, 124, -70, 26, 26, Color( 200, 0, 0 ) )
+		end
 	cam.End3D2D()
-	--cooler:Remove()
+
+	--[[-----------------------------------------
+		Player Name
+	--]]-----------------------------------------
+	Smooth = 80
+	Ang:RotateAroundAxis( Ang:Forward(), -45 )
+	cam.Start3D2D( Pos *1.0 + Ang:Up() * -4, Ang, 0.1 )
+		local tr = util.GetPlayerTrace( _player, ang )
+		local tr = util.TraceLine(tr)
+
+		local Cord
+		local t = {}
+			t.start = _player:GetShootPos()
+			t.endpos = _player:GetAimVector() * 128 + t.start
+			t.filter = _player
+		
+		local tr = util.TraceLine(t)
+
+		if tr.Entity == self then 
+			Cord = 80
+		else
+			Cord = 0
+		end
+
+		draw.RoundedBox( 0, -145, -180-Cord, 300, 70, Color(100,100,100) )
+		draw.RoundedBox( 0, -140, -175-Cord, 290, 60, Color(200,200,200) )
+		draw.SimpleText( owner, "MoneyFont", 0, -145-Cord, Color( 255, 120, 0 ), 1, 1 )
+	cam.End3D2D()
 end
--- entity/modules_money_printer/init.lua
+++ entity/modules_money_printer/init.lua
@@ -33,12 +33,12 @@ function ENT:Initialize()
 	self.maxUpgrade = 5
 	self.CoolDownOnUse = 0
 
-	self:SetNWBool( "turn", true )
-	self:SetNWInt("interval", MLevel[1][2])
-	self:SetNWInt("Upgrade", 0)
-	self:SetNWInt("paper", 5)
-	self:SetNWInt("paint", 10)
-	self:SetNWInt("Health", 100)
+	self:SetNWBool( "power", true )
+	self:SetNWInt( "interval", MLevel[1][2] )
+	self:SetNWInt( "Upgrade", 0 )
+	self:SetNWInt( "paper", 5 )
+	self:SetNWInt( "paint", 10 )
+	self:SetNWInt( "Health", 100 )
 end
 
 function ENT:SpawnFunction( ply, tr, ClassName )
@@ -50,35 +50,35 @@ function ENT:SpawnFunction( ply, tr, ClassName )
 	local ent = ents.Create( ClassName )
 	ent:SetPos( SpawnPos )
 	ent:SetAngles( Ang )
-	ent:Setowning_ent(ply)
+	ent:Setowning_ent( ply )
 	ent:Spawn()
 	ent:Activate()
 	return ent
 end
 
 function ENT:OnTakeDamage( Dmg )
-	self:SetNWInt( "Health", self:GetNWInt("Health") - Dmg:GetDamage())
-	if self:GetNWInt("Health") < 0 then
+	self:SetNWInt( "Health", self:GetNWInt( "Health" ) - Dmg:GetDamage())
+	if self:GetNWInt( "Health" ) < 0 then
 		self:Destruct()
 	end
 end
 
 function ENT:Destruct()
-	if self:GetNWInt("Health") <= 0 then
+	if self:GetNWInt( "Health" ) <= 0 then
 		local vPoint = self:GetPos()
 		local effectdata = EffectData()
-			effectdata:SetStart(vPoint)
-			effectdata:SetOrigin(vPoint)
-			effectdata:SetScale(1)
-		util.Effect("Explosion",effectdata)
+			effectdata:SetStart( vPoint )
+			effectdata:SetOrigin( vPoint )
+			effectdata:SetScale( 1 )
+		util.Effect( "Explosion", effectdata )
 		self:Remove()
 	end
 end
 
 function ENT:Extract()
-	if self:GetNWInt("Cooler") == false then return end
-	self:SetNWInt("Cooler", false )
-	local x,y,z = self:GetPos() + self:GetAngles():Forward() * 33
+	if self:GetNWBool( "Cooler" ) == false then return end
+	self:SetNWInt( "Cooler", false )
+	local x, y, z = self:GetPos() + self:GetAngles():Forward() * 33
 	local SpawnPos = Vector( x, y, z )
 	local ent = ents.Create( "module_cooler" )
 	ent:SetPos( SpawnPos )
@@ -87,46 +87,47 @@ function ENT:Extract()
 end
 
 function ENT:Try()
-	if self:GetNWInt("paper") > 0 and self:GetNWInt("paint") > 0 then
-		self:SetNWInt("paper", self:GetNWInt("paper")-1)
-		self:SetNWInt("paint", self:GetNWInt("paint")-1)
-		self:SetMoneyAmount(self:GetMoneyAmount() + self.giveMoney)
+	if self:GetNWInt( "paper" ) > 0 and self:GetNWInt( "paint" ) > 0 and 
+		self:GetMoneyAmount() ~= self.maxMoney then
+		self:SetNWInt( "paper", self:GetNWInt( "paper" ) - 1 )
+		self:SetNWInt( "paint", self:GetNWInt( "paint" ) - 1 )
+		self:SetMoneyAmount( self:GetMoneyAmount() + self.giveMoney )
 	end
 
-	if self:GetNWInt("Hot") < 100 and self:GetNWBool("Cooler") == false then
-		self:SetNWInt("Hot",self:GetNWInt("Hot") + 10)
-	else self:SetNWInt("ignite",1) end
+	if self:GetNWInt( "Hot" ) < 100 and self:GetNWBool( "Cooler" ) == false then
+		self:SetNWInt( "Hot", self:GetNWInt( "Hot" ) + 10 )
+	else self:SetNWInt( "ignite", 1 ) end
 
-	if self:GetNWBool("Cooler") == true and self:GetNWInt("Hot") > 0 then
-		self:SetNWInt("Hot", self:GetNWInt("Hot") - HotDecr)
-		if self:GetNWInt("Hot") < 0 then self:SetNWInt("Hot", 0) end
+	if self:GetNWBool( "Cooler" ) == true and self:GetNWInt( "Hot" ) > 0 then
+		self:SetNWInt( "Hot", self:GetNWInt( "Hot" ) - HotDecr )
+		if self:GetNWInt( "Hot" ) < 0 then self:SetNWInt( "Hot", 0 ) end
 	end
 end
 
 function ENT:Think()
 	if self.CoolDownOnUse > 0 then self.CoolDownOnUse = self.CoolDownOnUse - 1 end
-	if self:GetNWInt("Hot") >= 100 and self:GetNWBool("turn") == false then
-		self:SetNWInt("Health", self:GetNWInt("Health") -1 )
-		if self:GetNWInt("Health") <= 0 then self:Destruct() end
+	if self:GetNWInt( "Hot" ) >= 100 and self:GetNWBool( "power" ) == false then
+		self:SetNWInt( "Health", self:GetNWInt( "Health" ) - 1 )
+		if self:GetNWInt( "Health" ) <= 0 then self:Destruct() end
 	end
 	--ProgressBar
-	if (CurTime() - self.timer)/self:GetNWInt("interval") > 1 then
-		self:SetNWFloat("ProgressBar", 1)
+	if ( CurTime() - self.timer ) / self:GetNWInt( "interval" ) > 1 then
+		self:SetNWFloat( "ProgressBar", 1 )
 	else
-		if self:GetNWBool("turn") then
+		if self:GetNWBool( "power" ) then
 			LocalTime = CurTime() - self.timer
-			self:SetNWFloat("ProgressBar", LocalTime/self:GetNWInt("interval"))
+			self:SetNWFloat( "ProgressBar", LocalTime / self:GetNWInt( "interval" ) )
 		else self.timer = CurTime() - LocalTime end
 	end
 
-	if CurTime() >= self.timer + self:GetNWInt("interval") then
+	if CurTime() >= self.timer + self:GetNWInt( "interval" ) then
 		self.timer = CurTime()
 		if self:GetMoneyAmount() < self.maxMoney then
 			self:Try()
 		end
 	end
 	if self:GetMoneyAmount() > self.maxMoney then
-		self:SetMoneyAmount(self.maxMoney)
+		self:SetMoneyAmount( self.maxMoney )
 	end
 end
 
@@ -134,61 +135,64 @@ function ENT:Use( act, call )
 	if not call == call:IsPlayer() then return end
 
 	local Ang = self:GetAngles()
-	local Ply = act
 	
-	local tr = util.GetPlayerTrace( Ply, ang )
-	local tr = util.TraceLine(tr)
+	local tr = util.GetPlayerTrace( act, ang )
+	local tr = util.TraceLine( tr )
 
 	local t = {}
-		t.start = Ply:GetShootPos()
-		t.endpos = Ply:GetAimVector() * 128 + t.start
-		t.filter = Ply
+		t.start = act:GetShootPos()
+		t.endpos = act:GetAimVector() * 128 + t.start
+		t.filter = act
 		
-	local tr = util.TraceLine(t)
+	local tr = util.TraceLine( t )
 
-	local pos = self.Entity:WorldToLocal(tr.HitPos)
+	local pos = self.Entity:WorldToLocal( tr.HitPos )
 	local money = self:GetMoneyAmount()
 
-	if tr.Entity == self and pos.x > 6.5 and pos.x < 14 and pos.y > 11 and 
-		pos.y < 14.5 and self.CoolDownOnUse == 0 then
-		if self:GetNWBool("turn") == true then
-			self:SetNWBool( "turn", false )
+	if tr.Entity == self and pos.y > 12.3 and pos.y < 15 and pos.z > 4.4 and 
+		pos.z < 9.6 and self.CoolDownOnUse == 0 then
+		if self:GetNWBool( "power" ) == true then
+			self:SetNWBool( "power", false )
 		else
-			self:SetNWBool( "turn", true )
+			self:SetNWBool( "power", true )
 		end
-	elseif tr.Entity == self and pos.x > 6.5 and pos.x < 9.5 and pos.y > -14.5 and 
-		pos.y < -6 and self.CoolDownOnUse == 0 then
+		self.CoolDownOnUse = 5
+	elseif tr.Entity == self and pos.y > -13.5 and pos.y < 11 and pos.z > 4.4 and 
+		pos.z < 9.55 and self.CoolDownOnUse == 0 then
 		self:Extract()
+		self.CoolDownOnUse = 5
 	elseif money > 0 and self.CoolDownOnUse == 0 then
-		DarkRP.notify( call, 0, 4, "Вы взяли "..money.."$ из принтера!" )
+		DarkRP.notify( call, 0, 4, "You take "..money.."$ from Money Printer!" )
 		self:SetMoneyAmount(0)
-		Ply:addMoney(money)
+		act:addMoney( money )
+		self.CoolDownOnUse = 5
 	end
-	self.CoolDownOnUse = 5
 end
 
 function ENT:module_upgrade( entity )
 	entity:Remove()
-	self:SetNWInt("Upgrade", self:GetNWInt("Upgrade") + 1)
-	self.giveMoney = MLevel[self:GetNWInt("Upgrade") + 1][1]
-	self:SetNWInt("interval", MLevel[self:GetNWInt("Upgrade") + 1][2])
-	self.maxMoney = MLevel[self:GetNWInt("Upgrade") + 1][1] * MaxFactor
+	self:SetNWInt( "Upgrade", self:GetNWInt( "Upgrade" ) + 1 )
+	self.giveMoney = MLevel[self:GetNWInt( "Upgrade" ) + 1][1]
+	self:SetNWInt( "interval", MLevel[self:GetNWInt( "Upgrade" ) + 1][2])
+	self.maxMoney = MLevel[self:GetNWInt( "Upgrade" ) + 1][1] * MaxFactor
 end
 
 function  ENT:StartTouch( entity )
-	if entity:GetClass() == "module_cooler" and self:GetNWBool("Cooler") == false then
+	if entity:GetClass() == "module_cooler" and 
+		self:GetNWBool( "Cooler" ) == false then
 		entity:Remove()
-		self:SetNWBool("Cooler",true)
-	elseif entity:GetClass() == "module_paper" and self:GetNWInt("paper") <= 5 then
+		self:SetNWBool( "Cooler", true )
+	elseif entity:GetClass() == "module_paper" and 
+		self:GetNWInt( "paper" ) <= 5 then
 		entity:Remove()
-		self:SetNWInt("paper", self:GetNWInt("paper") + 5)
-	elseif entity:GetClass() == "module_paint" and self:GetNWInt("paint") <= 10 then
+		self:SetNWInt( "paper", self:GetNWInt( "paper" ) + 5 )
+	elseif entity:GetClass() == "module_paint" and 
+		self:GetNWInt( "paint" ) <= 10 then
 		entity:Remove()
-		self:SetNWInt("paint", self:GetNWInt("paint") + 10)
-	elseif entity:GetClass() == "module_upgrade_lv1" and self:GetNWInt("Upgrade") == 0 then self:module_upgrade(entity)
-	elseif entity:GetClass() == "module_upgrade_lv2" and self:GetNWInt("Upgrade") == 1 then self:module_upgrade(entity)
-	elseif entity:GetClass() == "module_upgrade_lv3" and self:GetNWInt("Upgrade") == 2 then self:module_upgrade(entity)
-	elseif entity:GetClass() == "module_upgrade_lv4" and self:GetNWInt("Upgrade") == 3 then self:module_upgrade(entity)
-	elseif entity:GetClass() == "module_upgrade_lv5" and self:GetNWInt("Upgrade") == 4 then self:module_upgrade(entity)
+		self:SetNWInt( "paint", self:GetNWInt( "paint" ) + 10 ) end
+	for i=1,5 do
+		if entity:GetClass() == "module_upgrade_lv"..i and 
+			self:GetNWInt( "Upgrade" ) == i - 1	then
+		self:module_upgrade( entity ) end
 	end
 end
